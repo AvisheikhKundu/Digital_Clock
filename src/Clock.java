0caf6e5ba365b596b561697007a64c50614cfc97
AvisheikhKundu <avisheikh76@gmail.com>
2023-03-17 14:59:37 +0600

    new code

--- src/Clock.java
+++ src/Clock.java
@@ -3,3 +3,24 @@ import java.awt.*;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 }
+
+public class Clock extends JFrame {
+
+    Calendar calendar;
+    SimpleDateFormat timeFormat;
+    SimpleDateFormat dayFormat;
+    SimpleDateFormat dateFormat;
+
+    JLabel timeLabel;
+    JLabel dayLabel;
+    JLabel dateLabel;
+    String time;
+    String day;
+    String date;
+    Clock() {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Digital_Clock_by_AVISHEIKH");
+        this.setLayout(new FlowLayout());
+        this.setSize(350, 220);
+        this.setResizable(false);
+
